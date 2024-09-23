be8390ab1742b1cbcacfd56d322c364afa4f8656
Rifat Hasan <rifathasancse10@gmail.com>
2024-09-23 15:56:36 -0800

    Assignment compleate

--- JS/script.js
+++ JS/script.js
@@ -16,7 +16,8 @@ const quata_input_btn = document.getElementById('quata-input-btn');
 const main_section = document.getElementById('main-section');
 const history_section = document.getElementById('history-section');
 const lg_blog_btn = document.getElementById('lg-blog');
-
+const sm_blog_btn = document.getElementById('sm-blog');
+const modal = document.getElementById('modal');
 
 // Main Balnce..
 let balance = 50000;
@@ -32,11 +33,9 @@ function calculateBalance(previous_balance,current_donated_amount){
     if(Number.isInteger(current_donated_amount) && current_donated_amount>0){
         
          sum = previous_balance + current_donated_amount;
+         return sum;
     }
-    else{
-        alert('Inter valid amount..');
-    }
-    return sum;
+   
 }
 
 
@@ -44,53 +43,79 @@ function calculateBalance(previous_balance,current_donated_amount){
 let n_current_balance = 0;
 noyakhali_btn.addEventListener('click',()=>{
     
-    n_current_balance = calculateBalance(n_current_balance,parseFloat(noyakhali_input.value));
-    noyakhali_balance.innerText = n_current_balance;
-    balance = parseFloat(balance) - parseFloat(noyakhali_input.value);
-    main_balance.innerText = balance;
-    const box = document.createElement('div');
-    box.setAttribute('class', 'lg:w-2/3 mx-auto p-5 border border-black rounded-lg my-3')
-    const paragraph = document.createElement('p');
-    paragraph.setAttribute('class', 'text-center');
-    paragraph.innerText = `${noyakhali_input.value} is Donated for Flood at Noakhali, Bangladesh at ${now}`;
-    box.appendChild(paragraph);
-    history_section.appendChild(box);
-    noyakhali_input.value = '';
+    if(noyakhali_input.value>0){
+
+        n_current_balance = calculateBalance(n_current_balance,parseFloat(noyakhali_input.value));
+        noyakhali_balance.innerText = n_current_balance;
+        balance = parseFloat(balance) - parseFloat(noyakhali_input.value);
+        main_balance.innerText = balance;
+        
+        const box = document.createElement('div');
+        box.setAttribute('class', 'lg:w-2/3 mx-auto p-5 border border-black rounded-lg my-3')
+        const paragraph = document.createElement('p');
+        paragraph.setAttribute('class', 'text-center');
+        paragraph.innerText = `${noyakhali_input.value} is Donated for Flood at Noakhali, Bangladesh at ${now}`;
+        box.appendChild(paragraph);
+        history_section.appendChild(box);
+        modal.setAttribute('onclick',my_modal_5.showModal());
+        noyakhali_input.value = '';
+   }
+   else{
+       alert('Invalid Input Amount..');
+       noyakhali_input.value = '';
+   }
+    
 })
 
 // Feni section donation calculation....
 let feni_current_balance = 0;
 feni_input_btn.addEventListener('click',()=>{
-    feni_current_balance = calculateBalance(feni_current_balance,parseFloat(feni_input.value));
-    feni_balnce.innerText = feni_current_balance;
-    balance = parseFloat(balance) - parseFloat(feni_input.value);
-    main_balance.innerText = balance;
-    const box = document.createElement('div');
-    box.setAttribute('class', 'lg:w-2/3 mx-auto p-5 border border-black rounded-lg my-3')
-    const paragraph = document.createElement('p');
-    paragraph.setAttribute('class', 'text-center');
-    paragraph.innerText = `${feni_input.value} is Donated for Flood Relief in Feni,Bangladesh at ${now}`;
-    box.appendChild(paragraph);
-    history_section.appendChild(box);
-    feni_input.value = '';
+
+    if(feni_input.value>0){
+        feni_current_balance = calculateBalance(feni_current_balance,parseFloat(feni_input.value));
+        feni_balnce.innerText = feni_current_balance;
+        balance = parseFloat(balance) - parseFloat(feni_input.value);
+        main_balance.innerText = balance;
+        const box = document.createElement('div');
+        box.setAttribute('class', 'lg:w-2/3 mx-auto p-5 border border-black rounded-lg my-3')
+        const paragraph = document.createElement('p');
+        paragraph.setAttribute('class', 'text-center');
+        paragraph.innerText = `${feni_input.value} is Donated for Flood Relief in Feni,Bangladesh at ${now}`;
+        box.appendChild(paragraph);
+        history_section.appendChild(box);
+        modal.setAttribute('onclick',my_modal_5.showModal());
+        feni_input.value = '';
+    }
+    else{
+        alert('Invalid Input Amount..');
+        feni_input.value = '';
+    }
 })
 
 // Quata section calculation...
 let quata_current_balance = 0;
 quata_input_btn.addEventListener('click',()=>{
-    quata_current_balance = calculateBalance(quata_current_balance,parseFloat(quata_input.value));
-    quata_balance.innerText = quata_current_balance;
-    balance = parseFloat(balance) - parseFloat(quata_input.value);
-    main_balance.innerText = balance;
+    if(quata_input.value>0){
+        quata_current_balance = calculateBalance(quata_current_balance,parseFloat(quata_input.value));
+        quata_balance.innerText = quata_current_balance;
+        balance = parseFloat(balance) - parseFloat(quata_input.value);
+        main_balance.innerText = balance;
+    
+        const box = document.createElement('div');
+        box.setAttribute('class', 'lg:w-2/3 mx-auto p-5 border border-black rounded-lg my-3')
+        const paragraph = document.createElement('p');
+        paragraph.setAttribute('class', 'text-center');
+        paragraph.innerText = `${quata_input.value} is Donated for Aid for Injured in the Quota Movement at ${now}`;
+        box.appendChild(paragraph);
+        history_section.appendChild(box);
+        modal.setAttribute('onclick',my_modal_5.showModal());
+        quata_input.value = '';
+    }
 
-    const box = document.createElement('div');
-    box.setAttribute('class', 'lg:w-2/3 mx-auto p-5 border border-black rounded-lg my-3')
-    const paragraph = document.createElement('p');
-    paragraph.setAttribute('class', 'text-center');
-    paragraph.innerText = `${quata_input.value} is Donated for Aid for Injured in the Quota Movement at ${now}`;
-    box.appendChild(paragraph);
-    history_section.appendChild(box);
-    quata_input.value = '';
+    else{
+        alert('Invalid Input Amount..');
+        quata_input.value = '';
+    }
 })
 
 // Donation and History button...
@@ -111,4 +136,16 @@ donation.addEventListener('click',()=>{
 lg_blog_btn.addEventListener('click',()=>{
     window.location.href = 'blog.html';
 })
+
+sm_blog_btn.addEventListener('click',()=>{
+    window.location.href = 'blog.html';
+})
+
+
+
+
+
+
+
+
 
